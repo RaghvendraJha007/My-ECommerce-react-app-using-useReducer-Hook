f0137d2d9102a2b21fc7d4cdd44c190a3d81f9c1
RaghvendraJha007 <raghvendrajham@gmail.com>
2023-06-04 17:49:47 +0530

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useFilterContext } from '../context/filter_context';
+import GridView from './GridView';
+import ListView from './ListView';
+
+const ProductList = () => {
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (grid_view === false) {
+    return <ListView products={products} />;
+  }
+
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        Sorry, no products matched your search...
+      </h5>
+    );
+  }
+
+  return <GridView products={products}>product list</GridView>;
+};
+
+export default ProductList;
